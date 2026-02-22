f5495babca9750fd3011acc4772bbb3cad3b2ccd
Intars Voitkevičs <ivmagiccc2008@gmail.com>
2026-02-22 02:44:23 +0200

    Ieviestas bikses un cepures. Ieviesta drēbju kumodes drēbju izvēles loģika.

---
+++ Assets/Scripts/DrebjuKategorijasPoga.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DrebjuKategorijuPoga : MonoBehaviour
+{
+    [Header("Šīs kategorijas drēbes")]
+    public GameObject[] clothingItems;
+
+    private Toggle toggle;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+        SetOpen(false);
+        toggle.isOn = false;
+    }
+
+    void OnToggleChanged(bool isOn)
+    {
+        if (isOn)
+            DrebjuKategorijuMenedzeris.Instance.OnCategoryClicked(this);
+        else
+            SetOpen(false);
+    }
+
+    public void SetOpen(bool open)
+    {
+        // Mainām toggle vizuāli bez eventa triggerēšanas
+        toggle.onValueChanged.RemoveListener(OnToggleChanged);
+        toggle.isOn = open;
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+
+        foreach (var item in clothingItems)
+        {
+            if (item != null)
+                item.SetActive(open);
+        }
+    }
+}
---
+++ Assets/Scripts/DrebjuKategorijuMenedzeris.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DrebjuKategorijuMenedzeris : MonoBehaviour
+{
+    public static DrebjuKategorijuMenedzeris Instance;
+
+    private DrebjuKategorijuPoga currentActive = null; // <- Tagad pareizais tips
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnCategoryClicked(DrebjuKategorijuPoga clicked)
+    {
+        if (currentActive == clicked)
+        {
+            clicked.SetOpen(false);
+            currentActive = null;
+        }
+        else
+        {
+            if (currentActive != null)
+                currentActive.SetOpen(false);
+
+            clicked.SetOpen(true);
+            currentActive = clicked;
+        }
+    }
+}
