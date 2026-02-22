48d4c91c4ec4da071116ee1f456b1fe9de3693d2
Intars Voitkevičs <ivmagiccc2008@gmail.com>
2026-02-22 15:28:47 +0200

    Ieviests Scrollview, platuma un garuma slīdrāde, un vairāktēlu izvēle.

---
+++ Assets/Scripts/GalvenaTelaHorizUnVertik.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TelaIzmers : MonoBehaviour
+{
+    [Header("Spongebob tēls")]
+    public RectTransform spongebobRect;
+
+    [Header("Slideri")]
+    public Slider garumsSlider;
+    public Slider platumsSlider;
+
+    private Vector2 sakumaIzmers;
+
+    void Start()
+    {
+        sakumaIzmers = spongebobRect.sizeDelta;
+
+        garumsSlider.onValueChanged.AddListener(MainitGarumu);
+        platumsSlider.onValueChanged.AddListener(MainitPlatumu);
+    }
+
+    void MainitGarumu(float vertiba)
+    {
+        spongebobRect.sizeDelta = new Vector2(spongebobRect.sizeDelta.x, vertiba);
+    }
+
+    void MainitPlatumu(float vertiba)
+    {
+        spongebobRect.sizeDelta = new Vector2(vertiba, spongebobRect.sizeDelta.y);
+    }
+}
